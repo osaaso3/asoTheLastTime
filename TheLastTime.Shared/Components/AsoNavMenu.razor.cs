b9265f5fa630df48fc44cd9779be14c364b7054a
asohr <asohr@DESKTOP-FQ4SFVU>
2021-04-15 01:19:38 +0430

    i

---
+++ TheLastTime.Shared/Components/AsoNavMenu.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using TheLastTime.Shared.Data;
+
+namespace TheLastTime.Shared.Components
+{
+    public partial class AsoNavMenu
+    {
+        [Inject]
+        DataService DataService { get; set; } = null!;
+
+        [Inject]
+        State State { get; set; } = null!;
+
+        bool collapseNavMenu = true;
+
+        string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        async void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+
+            State.ShowOptions = false;
+
+            if (DataService.Settings.ShowHelp != false)
+            {
+                DataService.Settings.ShowHelp = false;
+                await DataService.SaveSettings();
+            }
+        }
+
+        async void ShowHelp()
+        {
+            State.ShowOptions = false;
+            collapseNavMenu = true;
+
+            if (DataService.Settings.ShowHelp != true)
+            {
+                DataService.Settings.ShowHelp = true;
+                await DataService.SaveSettings();
+            }
+        }
+
+        async void ToggleOptions()
+        {
+            State.ShowOptions = !State.ShowOptions;
+
+            collapseNavMenu = true;
+
+            if (DataService.Settings.ShowHelp != false)
+            {
+                DataService.Settings.ShowHelp = false;
+                await DataService.SaveSettings();
+            }
+        }
+    }
+}
